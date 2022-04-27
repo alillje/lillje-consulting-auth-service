74d12343dc01d7a00c5db4830bcba2dcf5ee54b8
Andreas Lillje <al225wg@student.lnu.se>
2022-04-27 14:04:07 +0200

    Set acces-controll-allow-origin policy

--- src/server.js
+++ src/server.js
@@ -27,14 +27,14 @@ try {
   app.use(helmet())
 
   // Add Access Control Allow Origin headers
-  // app.use((req, res, next) => {
-  //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-  //   res.header(
-  //     'Access-Control-Allow-Headers',
-  //     'Origin, X-Requested-With, Content-Type, Accept'
-  //   )
-  //   next()
-  // })
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN)
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
 
   // Set up a morgan logger using the dev format for log entries.
   app.use(logger('dev'))
