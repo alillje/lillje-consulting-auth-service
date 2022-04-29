fcbdca8a1a79fe0e5fc8dfd407b800a63d8aa3cc
Andreas Lillje <al225wg@student.lnu.se>
2022-04-29 15:22:02 +0200

    Auto deploy when push to main

--- src/controllers/api/account-controller.js
+++ src/controllers/api/account-controller.js
@@ -220,7 +220,7 @@ export class AccountController {
    * Logs user out by revoking refresh token.
    *
    * @param {object} req - Express request object.
-   * @param {object} res - Express response object.
+   * @param {object} res - Express response object..
    * @param {Function} next - Express next middleware function.
    */
   async logout (req, res, next) {
